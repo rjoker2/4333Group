b5855fca4ece3360b93f08fb01219dd6ef4efa35
marselina <marselinazarifullina0@gmail.com>
2025-01-27 22:17:24 +0300

    Changed an empty WPF project

--- 4333Project/4333Project/4333_Зарифуллина.xaml.cs
+++ 4333Project/4333Project/4333_Зарифуллина.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace _4333Project
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для _4333_Зарифуллина.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class _4333_Зарифуллина : Window
     {
-        public MainWindow()
+        public _4333_Зарифуллина()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            _4333_Зарифуллина _4333_Зарифуллина = new _4333_Зарифуллина();
-            _4333_Зарифуллина.Show();
-            this.Close();
         }
     }
 }
