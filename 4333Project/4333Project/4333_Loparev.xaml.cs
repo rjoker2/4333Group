0c0ca43c36d95ca131e92360dbc9d3dc7c95a0ba
egor <egorka_loparev@mail.ru>
2025-01-29 09:58:44 +0300

    Created a button that a window with information about the author.

---
+++ 4333Project/4333Project/4333_Loparev.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _4333Project
+{
+    /// <summary>
+    /// Логика взаимодействия для _4333_Loparev.xaml
+    /// </summary>
+    public partial class _4333_Loparev : Window
+    {
+        public _4333_Loparev()
+        {
+            InitializeComponent();
+        }
+    }
+}
